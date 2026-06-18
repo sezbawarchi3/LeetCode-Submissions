bda6613856b96c6d48c98e2edd78b1c4a5bb19f6
sezbawarchi3 <sahajdhawan.251cs151@nitk.edu.in>
2026-06-19 01:00:41 +0530

    0054-SPIRAL-MATRIX Stats: Time: 0 ms (100%), Space: 9.2 MB (80.3%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,37 +1,36 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int n = matrix.size(), m = matrix[0].size();
         int rowstart = 0, rowend = matrix.size()-1, colstart = 0, colend = matrix[0].size()-1, count = 0;
         vector<int> sol;
-        while (count < n*m){
+        while (count < matrix.size() * matrix[0].size()){
             for(int i(colstart); i <= colend; i++){
                 sol.push_back(matrix[rowstart][i]);
                 count++;
             }
             rowstart++;
-            if(count == n*m) break;
+            if(count == matrix.size() * matrix[0].size()) break;
             
             for(int i(rowstart); i <= rowend; i++){
                 sol.push_back(matrix[i][colend]);
                 count++;
             }
             colend--;
-            if(count == n*m) break;
+            if(count == matrix.size() * matrix[0].size()) break;
             
             for(int i(colend); i >= colstart; i--){
                 sol.push_back(matrix[rowend][i]);
                 count++;
             }
             rowend--;
-            if(count == n*m) break;
+            if(count == matrix.size() * matrix[0].size()) break;
             
             for(int i(rowend); i >= rowstart; i--){
                 sol.push_back(matrix[i][colstart]);
                 count++;
             }
             colstart++;
-            if(count == n*m) break;
+            if(count == matrix.size() * matrix[0].size()) break;
         }
         return sol;
     }
