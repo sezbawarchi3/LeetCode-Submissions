679f9916bb9577b782d58282ee65171f38bf69d2
sezbawarchi3 <sahajdhawan.251cs151@nitk.edu.in>
2026-06-25 17:01:53 +0530

    0136-SINGLE-NUMBER Stats: Time: 0 ms (100%), Space: 20.7 MB (57.85%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,17 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int, int> m;
-        for (int i{0}; i < nums.size(); i++){
-            m[nums[i]]++;
+        int ans{0};
+        for(int n: nums){
+            ans ^= n;
         }
-        auto it = m.begin();
-        while (it != m.end()){
-            if (it -> second == 1){
-                return it -> first;
-            }
-            it++;
-        }
-        return it -> first;
+        return ans;
     }
 };
