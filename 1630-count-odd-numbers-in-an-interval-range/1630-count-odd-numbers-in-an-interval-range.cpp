a35fa1689c3ad7be6ac4e4be2ceb49de7fe68b62
sezbawarchi3 <sahajdhawan.251cs151@nitk.edu.in>
2026-07-19 21:06:41 +0530

    1630-COUNT-ODD-NUMBERS-IN-AN-INTERVAL-RANGE Stats: Time: 0 ms (100%), Space: 7.8 MB (41.45%) - LeetHub

--- 1630-count-odd-numbers-in-an-interval-range/1630-count-odd-numbers-in-an-interval-range.cpp
+++ 1630-count-odd-numbers-in-an-interval-range/1630-count-odd-numbers-in-an-interval-range.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int countOdds(int low, int high) {
-        if (high - low == 1) return 1;
-        return (high - low) % 2 == 0 ? (low % 2 == 0 ? (high - low)/2 : (high - low)/2 +1) : (high - low)/2 +1;
+        int x = high -low;
+        if (x == 1) return 1;
+        return (x) % 2 == 0 ? (low % 2 == 0 ? x/2 : x/2 +1) : x/2 +1;
     }
 };
